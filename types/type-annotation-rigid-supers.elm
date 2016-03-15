5a018f6e9952c297c27773ec1e98ee69c6af6ae0
Ferdinand Niedermann <ferdinand.niedermann@simplificator.com>
2016-03-15 18:48:51 +0100

    Remove rigid supers example because it compiles now

--- types/type-annotation-rigid-supers.elm
+++
@@ -1,9 +0,0 @@
--- https://github.com/elm-lang/elm-compiler/issues/821
-
-comparePair : comparable -> comparable -> comparable1 -> comparable2 -> Bool
-comparePair a b x y =
-  a < b || x < y
-
-
-usage =
-  comparePair 1 2 "hi" "blah"
